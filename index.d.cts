559e7db5a0f8f82383eacb36582db269b7376367
Aaron Belenky <aaron.b@sealsecurity.io>
2026-04-14 12:11:46 +0300

    CVE-2025-27152

--- index.d.cts
+++ index.d.cts
@@ -374,6 +374,7 @@ declare namespace axios {
     url?: string;
     method?: Method | string;
     baseURL?: string;
+    allowAbsoluteUrls?: boolean;
     transformRequest?: AxiosRequestTransformer | AxiosRequestTransformer[];
     transformResponse?: AxiosResponseTransformer | AxiosResponseTransformer[];
     headers?: (RawAxiosRequestHeaders & MethodsHeaders) | AxiosHeaders;
--- index.d.ts
+++ index.d.ts
@@ -315,6 +315,7 @@ export interface AxiosRequestConfig<D = any> {
   url?: string;
   method?: Method | string;
   baseURL?: string;
+  allowAbsoluteUrls?: boolean;
   transformRequest?: AxiosRequestTransformer | AxiosRequestTransformer[];
   transformResponse?: AxiosResponseTransformer | AxiosResponseTransformer[];
   headers?: (RawAxiosRequestHeaders & MethodsHeaders) | AxiosHeaders;
